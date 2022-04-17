750b5f889a6f5e5c5f35842c89492de19c682f1b
nymur-rahman-nyar <nymur.nyar@gmail.com>
2022-04-17 18:17:59 +0600

    Added Search icon in MrMckenicProductSearchPage.kt

--- app/src/main/java/com/example/damodor/MrMckenicProductSearchPage.kt
+++ app/src/main/java/com/example/damodor/MrMckenicProductSearchPage.kt
@@ -2,10 +2,17 @@ package com.example.damodor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MrMckenicProductSearchPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mr_mckenic_product_search_page)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.mrmckenic_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
