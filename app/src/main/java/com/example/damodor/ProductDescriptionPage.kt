ce23cf6c629031ae276361ce04bf9c340db53a82
nymur-rahman-nyar <nymur.nyar@gmail.com>
2022-05-05 16:21:32 +0600

    ProductDescriptionPage.kt Created

---
+++ app/src/main/java/com/example/damodor/ProductDescriptionPage.kt
@@ -0,0 +1,11 @@
+package com.example.damodor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProductDescriptionPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_description_page)
+    }
+}
